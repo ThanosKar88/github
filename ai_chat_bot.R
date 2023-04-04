aaac981cda87c74bf7bd8eb7feae45aeff668bf0
ThanosKar88 <125705777+ThanosKar88@users.noreply.github.com>
2023-04-04 22:06:51 +0300

    Add files via upload

--- ai_chat_bot.R
+++ ai_chat_bot.R
@@ -1,51 +1,50 @@
+# Load necessary packages
 library(shiny)
-library(rasa)
+library(googleAuthR)
+library(googleCloudRunner)
 
-# Define the UI for the chatbot
+# Authenticate with GCP
+options(googleAuthR.scopes.selected = c("https://www.googleapis.com/auth/cloud-platform",
+                                        "https://www.googleapis.com/auth/dialogflow"))
+gar_auth()
+
+# Set up the Dialogflow CX project and credentials
+project_id <- "your_project_id"
+location_id <- "your_location_id"
+agent_id <- "your_agent_id"
+session_id <- paste("session_", as.numeric(Sys.time()), sep = "")
+client <- dfcx_auth(project_id, location_id, agent_id)
+
+# Define UI
 ui <- fluidPage(
   titlePanel("AI Chatbot"),
   sidebarLayout(
     sidebarPanel(
-      textInput("user_input", "User Input"),
-      actionButton("send_button", "Send")
+      textInput("user_input", "Enter your message:"),
+      actionButton("send", "Send")
     ),
     mainPanel(
-      verbatimTextOutput("chat_output")
+      textOutput("bot_output")
     )
   )
 )
 
-# Define the server for the chatbot
+# Define server
 server <- function(input, output) {
-  
-  # Initialize the chatbot
-  rasa_agent <- load_agent("path/to/your/rasa/model")
-  
-  # Define a function to process the user input and generate a response
-  get_chat_response <- function(user_input) {
-    response <- rasa_agent %>% parse(user_input) %>% get_response()
-    return(response$text)
+  # Define function to handle user input
+  process_input <- function(input_text) {
+    # Send user input to Dialogflow CX for processing
+    query_input <- dfcx_text_query_input(input_text)
+    response <- dfcx_detect_intent(client, session_id, query_input)
+    response_text <- response$query_result$fulfillment_text
+    
+    return(response_text)
   }
   
-  # Define the output for the chatbot
-  chat_history <- reactiveValues(history = "")
-  
-  # Define a function to update the chat history with the user input and the bot response
-  update_chat_history <- function(user_input, bot_response) {
-    chat_history$history <- paste(chat_history$history, paste("User:", user_input), sep = "\n")
-    chat_history$history <- paste(chat_history$history, paste("Bot:", bot_response), sep = "\n")
-  }
-  
-  # Define a function to handle the send button
-  observeEvent(input$send_button, {
-    user_input <- input$user_input
-    bot_response <- get_chat_response(user_input)
-    update_chat_history(user_input, bot_response)
-  })
-  
-  # Define the output for the chatbot
-  output$chat_output <- renderPrint({
-    chat_history$history
+  # Define action for the send button
+  observeEvent(input$send, {
+    bot_output <- process_input(input$user_input)
+    output$bot_output <- renderText(bot_output)
   })
 }
 
